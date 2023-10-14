b4f388e664b76fcb2113304a9d450bf80acb8829
N2x4ZR <superdupercreativ3@gmail.com>
2023-10-14 22:36:54 +0300

    fixed calculator 2

--- js_les4_calculator-2/script.js
+++ js_les4_calculator-2/script.js
@@ -9,33 +9,30 @@ typeAction = prompt(`What action you want to do? Add, Diff, Mult, Div, Sqrt, Sin
 
 if (typeAction === null || !(typeAction === `Add` || typeAction === `Diff` || typeAction === `Mult` || typeAction === `Div` || typeAction === `Sqrt` || typeAction === `Sin` || typeAction === `Cos`)) {
    alert(`Good by, see you later.`);
-   process.exit(1);
-}
-
-if (typeAction === `Add` || typeAction === `Diff` || typeAction === `Mult` || typeAction === `Div`) {
-   numberA = parseFloat(prompt('Enter first number'));
-   if (isNaN(numberA)) {
-      alert(`This is bad digit, good by`);
-      process.exit(1);
-   } 
-   numberB = parseFloat(prompt('Enter second number'));
-   if (isNaN(numberB)) {
-      alert(`This is bad digit, good by`);
-      process.exit(1);
-   }
 } else {
-   numberA = parseFloat(prompt('Enter number'));
-   if (isNaN(numberA)) {
-      alert(`This is bad digit, good by`);
-      process.exit(1);
-   } 
+   if (typeAction === `Add` || typeAction === `Diff` || typeAction === `Mult` || typeAction === `Div`) {
+      numberA = parseFloat(prompt('Enter first number'));
+      if (isNaN(numberA)) {
+         alert(`This is bad digit, good by`);
+      } else {
+         numberB = parseFloat(prompt('Enter second number'));
+         if (isNaN(numberB)) {
+            alert(`This is bad digit, good by`);
+         } else {
+            if (typeAction === `Add`) alert(`Add of ${numberA} and ${numberB} is ${numberA + numberB}`);
+            if (typeAction === `Diff`) alert(`Diff of ${numberA} and ${numberB} is ${numberA - numberB}`);
+            if (typeAction === `Mult`) alert(`Mult of ${numberA} and ${numberB} is ${numberA * numberB}`);
+            if (typeAction === `Div`) alert(`Div of ${numberA} and ${numberB} is ${numberA / numberB}`);
+         }
+      }
+   } else {
+      numberA = parseFloat(prompt('Enter number'));
+      if (isNaN(numberA)) {
+         alert(`This is bad digit, good by`);
+      } else {
+         if (typeAction === `Sqrt`) alert(`Sqrt of ${numberA} is ${numberA * numberA}`);
+         if (typeAction === `Sin`) alert(`Sin of ${numberA} is ${Math.sin(numberA)}`);
+         if (typeAction === `Cos`) alert(`Cos of ${numberA} is ${Math.cos(numberA)}`);
+      }
+   }  
 }
-
-if (typeAction === `Add`) alert(`Add of ${numberA} and ${numberB} is ${numberA + numberB}`);
-if (typeAction === `Diff`) alert(`Diff of ${numberA} and ${numberB} is ${numberA - numberB}`);
-if (typeAction === `Mult`) alert(`Mult of ${numberA} and ${numberB} is ${numberA * numberB}`);
-if (typeAction === `Div`) alert(`Div of ${numberA} and ${numberB} is ${numberA / numberB}`);
-
-if (typeAction === `Sqrt`) alert(`Sqrt of ${numberA} is ${numberA * numberA}`);
-if (typeAction === `Sin`) alert(`Sin of ${numberA} is ${Math.sin(numberA)}`);
-if (typeAction === `Cos`) alert(`Cos of ${numberA} is ${Math.cos(numberA)}`);

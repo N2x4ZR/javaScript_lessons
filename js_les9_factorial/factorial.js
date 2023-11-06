80bed595ac301659fda816ffe3b9e43cea8fa537
N2x4ZR <superdupercreativ3@gmail.com>
2023-11-06 18:25:05 +0200

    add fractorial

--- js_les9_factorial/factorial.js
+++ js_les9_factorial/factorial.js
@@ -1,2 +1,12 @@
 'use strict';
 
+function factorial(n) {
+   if (n === 0 || n === 1) {
+      return 1;
+   } else {
+      return n * factorial(n - 1);
+   }
+}
+
+const a = factorial(6);
+console.log(a);

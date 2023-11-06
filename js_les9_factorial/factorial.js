a9f88c092903e5f4e6a77b2501f3e7c17841351d
N2x4ZR <superdupercreativ3@gmail.com>
2023-11-06 18:28:48 +0200

    fix

--- js_les9_factorial/factorial.js
+++ js_les9_factorial/factorial.js
@@ -8,5 +8,11 @@ function factorial(n) {
    }
 }
 
-const a = factorial(6);
+const a = factorial(2);
+const b = factorial(3);
+const c = factorial(4);
+const d = factorial(5);
 console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
